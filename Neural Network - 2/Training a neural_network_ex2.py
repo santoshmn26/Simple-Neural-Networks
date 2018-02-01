5d5f266fd4a3e38132f27eb9d9755a2e14b98321
Santosh Muniswamygowda Nagaraja <31931121+santoshmn26@users.noreply.github.com>
2018-01-31 23:30:16 -0500

    Update Training a neural_network_ex2.py

--- Neural Network - 2/Training a neural_network_ex2.py
+++ Neural Network - 2/Training a neural_network_ex2.py
@@ -1,39 +1,31 @@
 w1,w2,b=0.2,0.1,0.5
 x1=[0.5,0.5,1,1,1.5,1.5,2,2,2.5,2.5]
-x2=[2,2.2,2.3,2.5,2.4,2.6,2.5,2.7,2.6,2.8]
+x2=[2,2.4,2.4,2.5,2.4,2.6,2.5,2.7,2.6,2.8]
+y=[0,1,0,1,0,1,0,1,0,1]
 tx1=[1.5,1.5,2.5,2.5]
 tx2=[2.4,2.5,2.7,2.8]
-def update_w(x1,x2,w1,w2,b):
-    w=[]
+def update_w1(x1,x2,w1,w2,b,y):
     a=x1*w1+x2*w2+b
-    y=0.3*x1+2
-    if(y<x2):
-        y=1
-    else:
-        y=0
     gradw1=-1*(y-a)*x1
-    gradw2=-1*(y-a)*x2
     w1=w1-0.01*gradw1
-    w2=w2-0.01*gradw2
-    w.append(w1)
-    w.append(w2)
-    return w
+    return w1
+def update_w2(x1,x2,w1,w2,b,y):
+    a=x1*w1+x2*w2+b
+    gradw2=-1*(y-a)*x2
+    w2=w2-0.01*gradw1
+    return w2
 def update_b(x1,x2,w1,w2,b):
     a = x1 * w1 + x2 * w2 + b
-    y = 0.3 * x1 + 2
-    if (y < x2):
-        y = 1
-    else:
-        y = 0
     gradb=(-1 * (y - a))
     b = b-0.01*gradb
     return b
 for i in range(1000):#Epcoh
-    for i in range(9):
-        new_w=update_w(x1[i],x2[i],w1,w2,b)
+    for i in range(10):
+        new_w1=update_w1(x1[i],x2[i],w1,w2,b)
+        new_w2=update_w1(x1[i],x2[i],w1,w2,b)
         new_b=update_b(x1[i],x2[i],w1,w1,b)
-        w1=new_w[0]
-        w2=new_w[1]
+        w1=new_w1
+        w2=new_w2
         b=new_b
 print(w1,w2,b)
 while(True):
