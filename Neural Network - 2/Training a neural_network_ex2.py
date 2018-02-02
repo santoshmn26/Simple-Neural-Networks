c114dff81850804eb204ceaaf213d3aefd88cc96
Santosh Muniswamygowda Nagaraja <31931121+santoshmn26@users.noreply.github.com>
2018-02-01 23:14:58 -0500

    Update Training a neural_network_ex2.py

--- Neural Network - 2/Training a neural_network_ex2.py
+++ Neural Network - 2/Training a neural_network_ex2.py
@@ -1,39 +1,34 @@
-w1,w2,b=0.2,0.1,0.5
-x1=[0.5,0.5,1,1,1.5,1.5,2,2,2.5,2.5]
-x2=[2,2.4,2.4,2.5,2.4,2.6,2.5,2.7,2.6,2.8]
-y=[0,1,0,1,0,1,0,1,0,1]
-tx1=[1.5,1.5,2.5,2.5]
-tx2=[2.4,2.5,2.7,2.8]
-def update_w1(x1,x2,w1,w2,b,y):
-    a=x1*w1+x2*w2+b
+w1,w2,b=0.2,0.1,0.5 # initilize  the weights and Bias
+x1=[1,1,2,2,3,3,4,4,5,5]        # Train data for x1
+x2=[2.2,2.4,2.5,2.7,2.8,3,3,3.3,3.4,3.7]    # Train data for x2
+y=[0,1,0,1,0,1,0,1,0,1,0]       # Train data output
+tx1=[1.5,1.5,2.5,2.5]           # test data x1
+tx2=[2.4,2.5,2.7,2.8]           # test data x2
+def update_w1(x1,x2,w1,w2,b,y,a):   # update the weight for w1
     gradw1=-1*(y-a)*x1
-    w1=w1-0.01*gradw1
-    return w1
-def update_w2(x1,x2,w1,w2,b,y):
-    a=x1*w1+x2*w2+b
+    w=w1-(0.01*gradw1)
+    return w
+def update_w2(x1,x2,w1,w2,b,y,a):    # update the weight for w2
     gradw2=-1*(y-a)*x2
-    w2=w2-0.01*gradw1
-    return w2
-def update_b(x1,x2,w1,w2,b):
-    a = x1 * w1 + x2 * w2 + b
+    w=w2-(0.01*gradw2)
+    return w
+def update_b(x1,x2,w1,w2,b,y,a):      # update the weight for b
     gradb=(-1 * (y - a))
-    b = b-0.01*gradb
-    return b
-for i in range(1000):#Epcoh
+    b1 = b-(0.01*gradb)
+    return b1
+for i in range(1000):               #Epcoh
     for i in range(10):
-        new_w1=update_w1(x1[i],x2[i],w1,w2,b)
-        new_w2=update_w1(x1[i],x2[i],w1,w2,b)
-        new_b=update_b(x1[i],x2[i],w1,w1,b)
+        a = (x1[i] * w1) + (x2[i] * w2) + b     # calculating the actual output
+        new_w1=update_w1(x1[i], x2[i], w1, w2, b, y[i],a)   
+        new_w2=update_w2(x1[i], x2[i], w1, w2, b, y[i],a)
+        new_b=update_b(x1[i], x2[i], w1, w2, b, y[i],a)
         w1=new_w1
         w2=new_w2
         b=new_b
 print(w1,w2,b)
-while(True):
-    a=float(input())
-    c = float(input())
-    a1 = a * w1 + c * w2 + b
-    if(a>0.5):
-        print(1)
-    else:
-        print(0)
+while(True):        # Infinite loop to check the Neural network
+    c=float(input())   # input x1
+    d=float(input())   # input x2
+    a1 = c * w1 + d* w2 + b
+    print(a1)       # trained output
 
