d80d99d5fb6356da01e70e33d108cceb7e53f832
Santosh Muniswamygowda Nagaraja <31931121+santoshmn26@users.noreply.github.com>
2018-02-13 23:49:16 -0500

    Add files via upload

---
+++ Neural Network - 6 - using OOPS/Neuron class.py
@@ -0,0 +1,33 @@
+import math
+class Neuron:
+    weights=list()      
+    gradw=list()
+    gradb=0
+    bias=None
+    delta=None
+    s=None
+    a=None
+    fprime=None
+    inputs=list()
+    def computeSum(self,inputs):
+        self.sum=0
+        for i in range(int(len(self.weights))):
+            self.sum+=inputs[i]*self.weights[i]
+        self.s=self.sum+self.bias
+    def ComputeActivationFunctionDerivative(self):
+        self.fprime=self.a*(1-self.a)
+        return self.fprime
+    def Evaluate(self,inputs):
+        self.computeSum(inputs)
+        self.a=1/(1+math.exp(-1*self.s))
+        x=self.ComputeActivationFunctionDerivative()
+        return(self.a)
+'''
+x=Neuron()
+inputs=[1,2]
+x.inputs=inputs
+x.weights=[0.5,1.1]
+x.bias=1
+x.computeSum(inputs)
+print(x.s)
+'''
