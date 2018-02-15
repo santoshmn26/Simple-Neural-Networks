a58dc9412b9da813293a6b757a26d042ea2c36f6
Santosh Muniswamygowda Nagaraja <31931121+santoshmn26@users.noreply.github.com>
2018-02-14 23:31:27 -0500

    Update Layer_class.py

--- Neural Network - 6 - using OOPS/Layer_class.py
+++ Neural Network - 6 - using OOPS/Layer_class.py
@@ -1,7 +1,8 @@
-from Neuron_class import *
-class Layer(Neuron):
-    Neurons=list()
-    def layers(self,numNeurons):
+from Neuron_class import *                  #import the neuron class
+class Layer(Neuron):                        #use the Neuron class as the parent class
+    Neurons=list()                          #list to store the Neuron objects
+    def layers(self,numNeurons):            #Create a list of Neurons
         for i in range(numNeurons):
-            n=Neuron()
-            self.Neurons.append(n)
+            n=Neuron()                      
+            self.Neurons.append(n)          #Storing the Neurons object in the list of Neurons
+    
